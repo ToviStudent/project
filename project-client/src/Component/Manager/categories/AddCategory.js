1294bff7cb64e1b0b24a54e164de54541225d851
ToviStudent <36213814973@mby.co.il>
2023-04-19 21:06:18 +0300

    a

--- project-client/src/Component/Manager/categories/AddCategory.js
+++ project-client/src/Component/Manager/categories/AddCategory.js
@@ -2,21 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { ProductService } from './service/ProductService';
+import UseAxiosGet from '../../../hooks/UseAxiosGet';
 
 export default function AddCategory() {
-    const [products, setProducts] = useState([]);
+    // const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState(null);
 
+    const { data, loading, refetch, error } = UseAxiosGet('/manager/categories/another');
+    // const perUser = { identity: null, firstName: '', permission: "" };
     useEffect(() => {
-        ProductService.getProductsMini().then(data => setProducts(data));
-    }, []);
+        console.log('data', data);
+        if (data) 
+            setCategories(data)
+    }, [data])
+
+    // const array1 = ['a', 'b', 'c'];
+    // const iterator = array1.keys();
+    
+    // for (const key of iterator) {
+    //   console.log(key);
+    // }
+
+    // useEffect(() => {
+    //     ProductService.getProductsMini().then(data => setProducts(data));
+    // }, []);
 
     return (
         <div className="card">
-            <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
+            <DataTable value={categories} tableStyle={{ minWidth: '50rem' }}>
                 <Column field="code" header="Code"></Column>
                 <Column field="name" header="Name"></Column>
-                <Column field="category" header="Category"></Column>
-                <Column field="quantity" header="Quantity"></Column>
             </DataTable>
         </div>
     );
